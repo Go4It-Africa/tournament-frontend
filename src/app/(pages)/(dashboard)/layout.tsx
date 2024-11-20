f8f7e597b11cf295aa8386cafa3b282323876033
Norman Munge <normanmunge@gmail.com>
2024-11-20 07:20:31 +0300

    Styling changes

--- src/app/(pages)/(dashboard)/layout.tsx
+++ src/app/(pages)/(dashboard)/layout.tsx
@@ -1,7 +1,6 @@
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div>
-      Dashboard Layout
       <div>{children}</div>
     </div>
   );
---
+++ src/app/(pages)/(dashboard)/tournaments/layout.tsx
@@ -0,0 +1,9 @@
+const TournamentsLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div>
+      <div>{children}</div>
+    </div>
+  );
+};
+
+export default TournamentsLayout;
