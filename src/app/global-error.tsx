4bfbbed0bc873716428042bb887f7ddcc4a68621
Norman Munge <normanmunge@gmail.com>
2024-11-20 05:30:32 +0300

    Adds a global error file

---
+++ src/app/global-error.tsx
@@ -0,0 +1,27 @@
+'use client';
+
+// import { useEffect } from 'react';
+
+const Error = ({
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) => {
+  // useEffect(() => {
+  //   console.error(error);
+  // }, [error]);
+
+  return (
+    <>
+      <div>
+        <h2>Something went wrong!</h2>
+        <button onClick={() => reset()}>Try again!</button>
+      </div>
+    </>
+  );
+};
+
+export default Error;
