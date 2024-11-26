c5e69136339d361b45e31de123f09b89ef34b759
Norman Munge <normanmunge@gmail.com>
2024-11-26 04:38:06 +0300

    Hides tournament create & view pages

--- src/app/(pages)/(dashboard)/page.tsx
+++ src/app/(pages)/(dashboard)/page.tsx
@@ -5,14 +5,20 @@ const Home = () => {
   return (
     <>
       <div className='container'>
-        <Link href='/tournaments/create' className='button'>
+        {/* <Link href='/tournaments/create' className='button'>
           Register Tournament
-        </Link>
-        <Link href='/clubs/create' className='button'>
-          Register Club
         </Link>
         <Link href='/tournaments' className='button'>
           View Tournaments
+        </Link> */}
+        <Link href='/clubs/create' className='button'>
+          Register Club
+        </Link>
+        <Link href='/players/create' className='button'>
+          Register Player
+        </Link>
+        <Link href='/players' className='button'>
+          View Players
         </Link>
       </div>
     </>
