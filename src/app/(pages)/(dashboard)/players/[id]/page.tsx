6ecf3c562a074f44e74bab1cd7753ecacdba5f8c
Norman Munge <normanmunge@gmail.com>
2024-11-27 22:13:43 +0300

    Adds ui for player without stats

--- src/app/(pages)/(dashboard)/players/[id]/page.tsx
+++ src/app/(pages)/(dashboard)/players/[id]/page.tsx
@@ -69,32 +69,40 @@ const ViewPlayer = async (url: never) => {
         <div>
           <h3 className='text-left my-[1.25rem]'>Recent Trainings</h3>
         </div>
-        <div className='table-container'>
-          <table className='stats-table'>
-            <thead>
-              <tr>
-                <th className='sticky'>Type</th>
-                <th>Average Distance (km)</th>
-                <th>Top Speed (km/h)</th>
-                <th>Weight (kg)</th>
-                <th>Distance Per Minute</th>
-                <th>Work Ratio</th>
-                <th>Energy Exerted (kcal)</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td className='team sticky'>Game (90 mins)</td>
-                <td>{average_distance_km}</td>
-                <td>{top_speed}</td>
-                <td>{weight}</td>
-                <td>{energy_exerted_kcal}</td>
-                <td>{distance_per_minute}</td>
-                <td>{work_ratio}</td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
+        {average_distance_km && (
+          <div className='table-container'>
+            <table className='stats-table'>
+              <thead>
+                <tr>
+                  <th className='sticky'>Type</th>
+                  <th>Average Distance (km)</th>
+                  <th>Top Speed (km/h)</th>
+                  <th>Weight (kg)</th>
+                  <th>Distance Per Minute</th>
+                  <th>Work Ratio</th>
+                  <th>Energy Exerted (kcal)</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td className='team sticky'>Game (90 mins)</td>
+                  <td>{average_distance_km}</td>
+                  <td>{top_speed}</td>
+                  <td>{weight}</td>
+                  <td>{energy_exerted_kcal}</td>
+                  <td>{distance_per_minute}</td>
+                  <td>{work_ratio}</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        )}
+
+        {!average_distance_km && (
+          <div className='text-left'>
+            <p>No recent trainings found for this player.</p>
+          </div>
+        )}
 
         <Link href='/players' className='button'>
           <button>Go Back</button>
