48bf275d16348e30873f2d9703dc5c084d452759
Norman Munge <normanmunge@gmail.com>
2024-11-29 10:10:20 +0300

    Adds player interaface for ts typechecking

---
+++ src/types/data.ts
@@ -0,0 +1,12 @@
+export interface Player {
+  first_name: string;
+  last_name: string;
+  date_of_birth: string;
+  gender: string;
+  weight: number;
+  type_of_sport: string;
+  position: string;
+  country: string;
+  nationality: string;
+  club_id: number | string;
+}
