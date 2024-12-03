0a2ca29df7409cdbf6a44b89d3acdd9e49ce1b38
Norman Munge <normanmunge@gmail.com>
2024-12-03 13:43:42 +0300

    Adds login page forms

--- src/app/(pages)/auth/login/page.tsx
+++ src/app/(pages)/auth/login/page.tsx
@@ -1,5 +1,28 @@
+'use client';
+
+import { useActionState } from 'react';
+import { loginHandler } from '@/pages/auth/login/action';
+
 const Login = () => {
-  return <div>Login Page</div>;
+  const [state, formAction, pending] = useActionState(
+    (state: unknown, formData: FormData) => loginHandler(state, formData),
+    null
+  );
+
+  return (
+    <div>
+      <form action={formAction}>
+        <input type='email' name='email' placeholder='Email' />
+        {state?.errors?.email && <p>{state.errors.email}</p>}
+        <input type='text' name='password' placeholder='Password' />
+        {state?.errors?.password && <p>{state.errors.password}</p>}
+
+        <button disabled={pending} type='submit'>
+          {pending ? 'Logging In...' : 'Log In'}
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
