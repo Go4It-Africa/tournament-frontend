3408e597281df3175046df4708212e6c09e75e20
Norman Munge <normanmunge@gmail.com>
2024-12-12 12:45:37 +0300

    Updates the metrics for player

--- src/app/(pages)/(dashboard)/players/[id]/page.tsx
+++ src/app/(pages)/(dashboard)/players/[id]/page.tsx
@@ -30,7 +30,34 @@ const ViewPlayer = (url: never) => {
   const gender = searchParams.get('gender');
   const last_name = searchParams.get('last_name');
   const position = searchParams.get('position');
-  const type_of_sport = searchParams.get('type_of_sport');
+  const impact = searchParams.get('impact');
+  const duration = searchParams.get('duration');
+
+  type PlayerMetric = {
+    metric: string;
+    value: string | number | null;
+  };
+
+  console.log('searchParams', searchParams);
+
+  const playerMetrics: PlayerMetric[] = [
+    {
+      metric: 'Distance Covered (km)',
+      value: parseFloat(average_distance_km || '0'),
+    },
+    {
+      metric: 'Distance Covered Per Minute (metres/min)',
+      value: parseFloat(distance_per_minute || '0'),
+    },
+    { metric: 'Top Speed (metres/s)', value: parseFloat(top_speed || '0') },
+    { metric: 'Duration Played (min)', value: parseFloat(duration || '0') },
+    {
+      metric: 'Energy Exerted (kcal)',
+      value: parseFloat(energy_exerted_kcal || '0'),
+    },
+    { metric: 'Work Rate', value: parseFloat(work_ratio || '0') },
+    { metric: 'Impact', value: parseFloat(impact || '0') },
+  ];
 
   //const player = url['searchParams'];
 
@@ -58,85 +85,94 @@ const ViewPlayer = (url: never) => {
 
   return (
     <>
-      <div>
-        <div className='player-card'>
-          <div className='player-header'>
-            <div className='role-badge'>{position}</div>
-            <Image
-              src='/user.png'
-              width={500}
-              height={500}
-              alt='Player name'
-              className='mt-[3.125rem]'
-            />
-            <div className='player-details'>
-              <h3>
-                {first_name} {last_name}
-              </h3>
-              <p>{club_id}</p>
-            </div>
-          </div>
-          <div className='stats'>
-            <div className='stats-container'>
-              <div className='stat-item'>
-                <h4>Age</h4>
-                <p>{date_of_birth}</p>
-              </div>
-              <div className='stat-item'>
-                <h4>Gender</h4>
-                <p>{gender}</p>
-              </div>
-              <div className='stat-item'>
-                <h4>Nationality</h4>
-                <p>{nationality}</p>
-              </div>
-            </div>
-          </div>
-        </div>
+      {isClient && (
         <div>
-          <h3 className='text-left my-[1.25rem]'>Recent Trainings</h3>
+          <div className='player-card'>
+            <div className='player-header'>
+              <div className='role-badge'>{position}</div>
+              <Image
+                src='/user.png'
+                width={500}
+                height={500}
+                alt='Player name'
+                className='mt-[3.125rem]'
+              />
+              <div className='player-details'>
+                <h3>
+                  {first_name} {last_name}
+                </h3>
+                <p>{club_id}</p>
+              </div>
+            </div>
+            <div className='stats'>
+              <div className='stats-container'>
+                <div className='stat-item'>
+                  <h4>Age</h4>
+                  <p>{date_of_birth}</p>
+                </div>
+                <div className='stat-item'>
+                  <h4>Gender</h4>
+                  <p>{gender}</p>
+                </div>
+                <div className='stat-item'>
+                  <h4>Nationality</h4>
+                  <p>{nationality}</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div>
+            <h3 className='text-left my-[1.25rem]'>Recent Trainings</h3>
+          </div>
+          {average_distance_km && (
+            <div className='table-container'>
+              <table className='stats-table'>
+                <thead>
+                  <tr>
+                    <th className='sticky'>Type</th>
+                    <th>Average Distance (km)</th>
+                    <th>Top Speed (metres/s)</th>
+                    <th>Weight (kg)</th>
+                    <th>Distance Per Minute (metres/min)</th>
+                    <th>Work Ratio</th>
+                    <th>Energy Exerted (kcal)</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td className='team sticky'>Game</td>
+                    <td>{average_distance_km}</td>
+                    <td>{top_speed}</td>
+                    <td>{weight}</td>
+                    <td>{energy_exerted_kcal}</td>
+                    <td>{distance_per_minute}</td>
+                    <td>{work_ratio}</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          )}
+
+          {!average_distance_km && (
+            <div className='text-left'>
+              <p>No recent trainings found for this player.</p>
+            </div>
+          )}
+
+          {isClient && average_distance_km && (
+            <div className='w-full flex flex-col items-center'>
+              <h3 className='text-left my-[1.25rem]'>
+                Player Performance Metrics
+              </h3>
+              <RadialChart data={playerMetrics} />
+            </div>
+          )}
+
+          <Link href='/players' className='button'>
+            <button>Go Back</button>
+          </Link>
         </div>
-        {average_distance_km && (
-          <div className='table-container'>
-            <table className='stats-table'>
-              <thead>
-                <tr>
-                  <th className='sticky'>Type</th>
-                  <th>Average Distance (km)</th>
-                  <th>Top Speed (km/h)</th>
-                  <th>Weight (kg)</th>
-                  <th>Distance Per Minute</th>
-                  <th>Work Ratio</th>
-                  <th>Energy Exerted (kcal)</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td className='team sticky'>Game (90 mins)</td>
-                  <td>{average_distance_km}</td>
-                  <td>{top_speed}</td>
-                  <td>{weight}</td>
-                  <td>{energy_exerted_kcal}</td>
-                  <td>{distance_per_minute}</td>
-                  <td>{work_ratio}</td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-        )}
-
-        {!average_distance_km && (
-          <div className='text-left'>
-            <p>No recent trainings found for this player.</p>
-          </div>
-        )}
-
-        {isClient && <RadialChart />}
-
-        <Link href='/players' className='button'>
-          <button>Go Back</button>
-        </Link>
-      </div>
+      )}
     </>
   );
 };
