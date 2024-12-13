55ede67474cf7fcbe332f5ca78a7d0fb728fbf2e
Norman Munge <normanmunge@gmail.com>
2024-12-14 02:17:13 +0300

    Creates Navigation Component

---
+++ src/app/_components/Nav.tsx
@@ -0,0 +1,15 @@
+const Nav = () => {
+  return (
+    <nav>
+      <ul>
+        <li>Home</li>
+        <li>Club</li>
+        <li>Players</li>
+        <li>Schedule</li>
+        <li>Tournaments</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Nav;
